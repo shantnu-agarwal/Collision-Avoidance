4bebfd3b333221e2500b13dd86a0d618f8b22216
Shantnu Agarwal <shantnu.agarwal@gmail.com>
2018-09-27 21:27:14 +0530

    Detects front is clear or not

---
+++ Sketch1.ino
@@ -0,0 +1,111 @@
+#define radius 5    //cm
+#define theta 0.523   //radians
+#define halfwidth 15  //cm
+#define length 25     //cm
+#define NOS 5     //Number of sensors
+int trig[] = { 13,12,11,10,9 };
+int echo[] = { 7,6,5,4,3 };
+int i;
+
+
+
+class Data {
+
+	int raw[NOS];         //Holds raw values from the sensor in CMs
+	int horz[NOS];          //Holds calculated horizontal distances from the sensor. 
+	long duration;          //This variable is used in getInput() to store pulsein
+	long distance;          //This variable is used in getInput() to store converted distance
+	int clearSide[NOS];       //Sets 0 for not clear, 1 for clear
+
+
+
+public:
+	void getInput() {
+		for	(int i = 0; i < NOS; i++) {
+
+			digitalWrite(trig[i], LOW);
+			delayMicroseconds(5);
+			digitalWrite(trig[i], HIGH);
+			delayMicroseconds(5);
+			digitalWrite(trig[i], LOW);
+			duration = pulseIn(echo[i], HIGH);
+			distance = duration * 0.034 / 2;
+			if (distance > 150)
+				distance = 150;
+			raw[i] = distance;
+
+			horz[i] = abs((raw[i] + radius)*cos(((PI - (4 * theta)) / 2) + (i*theta)));
+		}
+
+
+	}
+	void showRaw()
+	{
+		for (i = 0; i < NOS; i++)
+		{
+			Serial.print(i);
+			Serial.print(" : ");
+			Serial.println(raw[i]);
+		}
+	}
+	void showHorz() {
+		for (i = 0; i < NOS; i++) {
+			Serial.print(i);
+			Serial.print(" : ");
+			Serial.println(horz[i]);
+		}
+	}
+	void isClear()
+	{
+		int temp = 0;
+		for (i = 0; i < NOS; i++)
+		{
+			if (i == NOS/2)
+			{
+				if (raw[i] > length)
+					clearSide[i] = 1;
+				else
+				{
+					clearSide[i] = 0;
+					temp++;
+				}
+				i++;
+			}
+			if (horz[i] > halfwidth)
+				clearSide[i] = 1;
+			else
+			{
+				clearSide[i] = 0;
+				temp++;
+			}
+		}
+
+		if (temp == 0)
+			Serial.println("Front Side Clear!!!!");
+		else
+			Serial.println("Can't go Straight !!!!!!!");
+	}
+
+	void chooseDirection(){
+	
+	}
+}obj;
+void setup() {
+
+	for (i = 0; i < NOS; i++) {
+
+		pinMode(trig[i], OUTPUT);
+		pinMode(echo[i], INPUT);
+	}
+	Serial.begin(9600);
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	obj.getInput();
+	//obj.showRaw();
+	obj.showHorz();
+	obj.isClear();
+	//obj.chooseDirection();
+	delay(500);
+}
