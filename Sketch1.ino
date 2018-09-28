8e12f557c08c185a3dd5f3de3bf6dd1cb48727a6
Shantnu Agarwal <shantnu.agarwal@gmail.com>
2018-09-28 21:42:38 +0530

    Logic only, without motor motion

--- Sketch1.ino
+++ Sketch1.ino
@@ -6,165 +6,196 @@
 int trig[] = { 13,12,11,10,9 };
 int echo[] = { 7,6,5,4,3 };
 int i;
-//add from VS direct
+
 
 class Data
 {
-  int raw[NOS];         //Holds raw values from the sensor in CMs
-  int horz[NOS];          //Holds calculated horizontal distances from the sensor. 
-  long duration;          //This variable is used in getInput() to store pulsein
-  long distance;          //This variable is used in getInput() to store converted distance
-  int clearSide[NOS];       //Sets 0 for not clear, 1 for clear
+	int raw[NOS];         //Holds raw values from the sensor in CMs
+	int horz[NOS];          //Holds calculated horizontal distances from the sensor. 
+	long duration;          //This variable is used in getInput() to store pulsein
+	long distance;          //This variable is used in getInput() to store converted distance
+	int clearSide[NOS];       //Sets 0 for not clear, 1 for clear
 public:
-  void getInput();
-  void showRaw();
-  void showHorz();
-  void isClear(); 
-  void chooseDirection(); 
+	void getInput();
+	void showRaw();
+	void showHorz();
+	void isClear();
+	void chooseDirection();
 }obj;
 void Data::getInput()
 {
-    for (int i = 0; i < NOS; i++)
-    {
-      digitalWrite(trig[i], LOW);
-      delayMicroseconds(5); 
-      digitalWrite(trig[i], HIGH);
-      delayMicroseconds(5);
-      digitalWrite(trig[i], LOW);
-      duration = pulseIn(echo[i], HIGH);
-      distance = duration * 0.034 / 2;
-      if (distance > 150)
-        distance = 150;
-      raw[i] = distance;
-      horz[i] = abs((raw[i] + radius)*cos(((PI - (4 * theta)) / 2) + (i*theta)));
-    }
-  }
-  void Data::showRaw()
-  {
-    for (i = 0; i < NOS; i++) 
-    {
-      Serial.print(i);
-      Serial.print(" : ");
-      Serial.println(raw[i]);
-    }
-  }
-  void Data::showHorz() 
-  {
-    for (i = 0; i < NOS; i++)
-     {
-      Serial.print(i);
-      Serial.print(" : ");
-      Serial.println(horz[i]);
-    }
-  }
-  void Data::isClear() 
-  {
-    int temp=0;
-    for (i = 0; i < NOS; i++) 
-    {
-      if(i==2)
-      {
-       if(raw[i]>length)
-        clearSide[i]=1; 
-       else
-        {
-        clearSide[i]=0;
-        temp++;
-        }
-       i++;
-      }
-      if (horz[i] > halfwidth)
-        clearSide[i] = 1;
-      else
-        {
-         clearSide[i] = 0;
-         temp++;        
-        }
-      }
-   if(temp==0)
-    Serial.println("Front Side Clear!!!!")
-  }
-void Data::chooseDirection() 
-{
- int direction=0;
- double d1=0,d2=0;
- d1=(raw[0]+raw[1])/2;
- d2=(raw[3]+raw[4])/2;
- if(d1>d2) 
-  direction=1;
- else
-  direction=-1;
-
-}
-void setup() 
-{
-  for (i = 0; i < NOS; i++)
-  {
-    pinMode(trig[i], OUTPUT);
-    pinMode(echo[i], INPUT);
-  }
-  Serial.begin(9600);
-}
-// the loop function runs over and over again until power down or reset
-void loop() 
-{
-  obj.getInput();
-  obj.showRaw(); 
-  obj.showHorz();
-  //obj.isClear();
-  //obj.chooseDirection();
-}
-	void isClear()
+	for (int i = 0; i < NOS; i++)
 	{
-		int temp = 0;
-		for (i = 0; i < NOS; i++)
+		digitalWrite(trig[i], LOW);
+		delayMicroseconds(5);
+		digitalWrite(trig[i], HIGH);
+		delayMicroseconds(5);
+		digitalWrite(trig[i], LOW);
+		duration = pulseIn(echo[i], HIGH);
+		distance = duration * 0.034 / 2;
+		if (distance > 150)
+			distance = 150;
+		raw[i] = distance;
+		horz[i] = abs((raw[i] + radius)*cos(((PI - (4 * theta)) / 2) + (i*theta)));
+	}
+}
+void Data::showRaw()
+{
+	for (i = 0; i < NOS; i++)
+	{
+		Serial.print(i);
+		Serial.print(" : ");
+		Serial.println(raw[i]);
+	}
+}
+void Data::showHorz()
+{
+	for (i = 0; i < NOS; i++)
+	{
+		Serial.print(i);
+		Serial.print(" : ");
+		Serial.println(horz[i]);
+	}
+}
+void Data::isClear()
+{
+	int temp = 0;
+	for (i = 0; i < NOS; i++)
+	{
+		if (i == 2)
 		{
-			if (i == NOS/2)
-			{
-				if (raw[i] > length)
-					clearSide[i] = 1;
-				else
-				{
-					clearSide[i] = 0;
-					temp++;
-				}
-				i++;
-			}
-			if (horz[i] > halfwidth)
+			if (raw[i] > length)
 				clearSide[i] = 1;
 			else
 			{
 				clearSide[i] = 0;
 				temp++;
 			}
+			i++;
 		}
-
-		if (temp == 0)
-			Serial.println("Front Side Clear!!!!");
+		if (horz[i] > halfwidth)
+			clearSide[i] = 1;
 		else
-			Serial.println("Can't go Straight !!!!!!!");
+		{
+			clearSide[i] = 0;
+			temp++;
+		}
 	}
+	if (temp == 0)
+		Serial.println("Front Side Clear!!!!")
+}
+void Data::chooseDirection()
+{
+	int direction = 0;
+	double d1 = 0, d2 = 0;
+	d1 = (raw[0] + raw[1]) / 2;
+	d2 = (raw[3] + raw[4]) / 2;
+	if (d1 > d2)
+		direction = 1;
+	else
+		direction = -1;
 
-	void chooseDirection(){
-	
-	}
-}obj;
-void setup() {
-
-	for (i = 0; i < NOS; i++) {
-
+}
+void setup()
+{
+	for (i = 0; i < NOS; i++)
+	{
 		pinMode(trig[i], OUTPUT);
 		pinMode(echo[i], INPUT);
 	}
 	Serial.begin(9600);
 }
+// the loop function runs over and over again until power down or reset
+void loop()
+{
+	obj.getInput();
+	obj.showRaw();
+	obj.showHorz();
+	//obj.isClear();
+	//obj.chooseDirection();
+}
+void isClear()
+{
+	int temp = 0;
+	for (i = 0; i < NOS; i++)
+	{
+		if (i == NOS / 2)
+		{
+			if (raw[i] > length)
+				clearSide[i] = 1;
+			else
+			{
+				clearSide[i] = 0;
+				temp++;
+			}
+			i++;
+		}
+		if (horz[i] > halfwidth)
+			clearSide[i] = 1;
+		else
+		{
+			clearSide[i] = 0;
+			temp++;
+		}
+	}
+	if (temp == 0)
+		Serial.println("Front Side Clear!!!!");
+	else
+		Serial.println("Can't go Straight !!!!!!!");
+}
+void chooseDirection() {
 
+	int dr = 1; //by default direction is set right
+	float ls = 0, rs = 0, lh = 0, rh = 0;
+	for (int i = 0; i < NOS; i++) //to calculate average sum of ls,lh,rs,rh
+	{
+		if (i < NOS / 2)
+		{
+			lh = horz[i];
+			ls = raw[i];
+		}
+		if (i > NOS / 2)
+		{
+			rh = horz[i];
+			rs = raw[i];
+		}
+	}
+	ls /= NOS / 2;
+	rs /= NOS / 2;
+	lh /= NOS / 2;
+	rh /= NOS / 2;
+	if (ls > rs)
+		dr = -1; //if left path is a better path to be followed
+
+	if (dr = -1) //left motion
+	{
+		if (lh < halfwidth)
+			left();
+		else
+			stopp();
+	}
+	else if (dr = 1) //right motion
+	{
+		if (rh < halfwidth)
+			right();
+		else
+			stopp();
+	}
+}
+}obj;
+void setup() {
+	for (i = 0; i < NOS; i++) {
+		pinMode(trig[i], OUTPUT);
+		pinMode(echo[i], INPUT);
+	}
+	Serial.begin(9600);
+}
 // the loop function runs over and over again until power down or reset
 void loop() {
 	obj.getInput();
 	//obj.showRaw();
 	obj.showHorz();
 	obj.isClear();
-	//obj.chooseDirection();
+	obj.chooseDirection();
 	delay(500);
 }
