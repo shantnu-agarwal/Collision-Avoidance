3fcb73e5b96076ac85368f2b17b2f7997715d450
Shantnu Agarwal <shantnu.agarwal@gmail.com>
2018-10-08 18:26:40 +0530

    FINAL

--- Sketch1.ino
+++ Sketch1.ino
@@ -1,9 +1,9 @@
 const int pwm[4] = { 5,6,7,8 };
 const int m[4][2] = { {51,53},{47,49},{22,24},{26,28} };
-#define radius 5    //cm
-#define theta 0.523   //radians
-#define halfwidth 50  //cm
-#define length 60    //cm
+#define radius 5    //cm // old 5
+#define theta 0.523   //radians // old 0.523
+#define halfwidth 45  //cm
+#define length 45    //cm
 #define NOS 5    //Number of sensors
 #define SPEED 100 //Sets the PWM speed for motors.
 int trig[] = { 52,50,34,32,30 };  //same
@@ -21,7 +21,7 @@ class Data {
   int horz[NOS];          //Holds calculated horizontal distances from the sensor. 
   long duration;          //This variable is used in getInput() to store pulsein
   long distance;          //This variable is used in getInput() to store converted distance
-  int clearSide[NOS];       //Sets 0 for not clear, 1 for clear
+ // int clearSide[NOS];       //Sets 0 for not clear, 1 for clear
 
 
 
@@ -69,20 +69,17 @@ public:
     {
       if (i == NOS / 2)
       {
-        if (raw[i] > length)
-          clearSide[i] = 1;
-        else
+        if (raw[i] < length)
         {
-          clearSide[i] = 0; /*THIS SIDE IS NOT CLEAR, OBJECT IS THERE*/
+           /*THIS SIDE IS NOT CLEAR, OBJECT IS THERE*/
           temp++;
         }
         i++;
       }
-      if (horz[i] > halfwidth) /*DETECTED OBJECT WILL NOT COLLIDE*/
-        clearSide[i] = 1;
-      else
+      if (horz[i] < halfwidth) /*DETECTED OBJECT WILL NOT COLLIDE*/
+        
       {
-        clearSide[i] = 0;
+        
         temp++;
       }
     }
@@ -119,9 +116,13 @@ public:
     rs /= NOS / 2;
     lh /= NOS / 2;
     rh /= NOS / 2;
-    if (ls > rs)    /*********COMPARING FEASIBILTY OF LEFT OVER RIGHT********/
+    int difference = ls-rs; //+ve when left is more clear
+    
+    if (difference < 25 && difference > -25)    /*********COMPARING FEASIBILTY OF LEFT OVER RIGHT********/
+      right();  
+    else if(difference > 25 )
       left();
-    else
+    else 
       right();
 
     /********************** DIRECTION OF MOTION HAS NOW BEEN DECIDED********************/
@@ -199,7 +200,7 @@ void setup() {
 }
 
 void loop() {
-  obj.showRaw();
+  //obj.showRaw();
   //obj.showHorz();
   obj.isClear();
 
